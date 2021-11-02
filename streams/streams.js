878e4589d5f7f1698e0063bd722186c803f8f863
mubashir <mubashir11131719@gmail.com>
2021-11-02 17:44:28 +0530

    done

--- streams/streams.js
+++ streams/streams.js
@@ -13,3 +13,21 @@ const writePoemStream =
 const transformStream = ???
 readPoemStream.pipe(transformStream).pipe(writePoemStream)
 */
+/*
+  Copy over to streams.js to test
+*/
+
+const readPoemStream = fs.createReadStream('on-joy-and-sorrow-emoji.txt');
+const writePoemStream = fs.createWriteStream('on-joy-and-sorrow-fixed.txt');
+
+function write(buffer, encoding, next) {
+  let text = buffer.toString();
+  text = text.replace(/:\)/gi, 'joy');
+  text = text.replace(/:\(/gi, 'sorrow');
+  this.push(text);
+  next();
+}
+
+const transformStream = through(write);
+
+readPoemStream.pipe(transformStream).pipe(writePoemStream);
